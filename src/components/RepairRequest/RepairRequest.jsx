6ffcf4d609921ad3530409b37c84391bd7338dce
baekseojin <seojinbaek100@gmail.com>
2023-12-14 17:04:38 +0900

    FEAT :: 버튼 누르면 페이지 이동 기능 추가

--- src/components/RepairRequest/RepairRequest.jsx
+++ src/components/RepairRequest/RepairRequest.jsx
@@ -1,36 +1,81 @@
 import styled from "styled-components";
+import { useState } from "react";
+import Header from "../Header/Header";
 
-export default function RepairRequest() {
+const RepairRequest = () => {
+  const [page, setPage] = useState(1);
+
+  const buttonClick = () => {
+    setPage(page + 1);
+  };
+
+  const backButtonClick = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const renderPage = () => {
+    switch (page) {
+      case 1:
+        return (
+          <div>
+            <Title>출발지 선택</Title>
+            <SubTitle>고객님의 의류의 출발지를 선택해주세요.</SubTitle>
+            <SelectButton>쇼핑몰에서 보내요</SelectButton>
+            <SelectButton>우리집에서 보내요</SelectButton>
+            <Button onClick={buttonClick}>다음 단계</Button>
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <Title>의류 선택</Title>
+            <SubTitle>어떤 옷을 수선하고 싶으세요?</SubTitle>
+            <SelectButton>하의</SelectButton>
+            <SelectButton>상의</SelectButton>
+            <SelectButton>아우터</SelectButton>
+            <SelectButton>원피스</SelectButton>
+            <SelectButton>정장/교복</SelectButton>
+            <Button onClick={buttonClick}>다음 단계</Button>
+          </div>
+        );
+      // 다른 페이지에 대한 내용 추가
+      default:
+        return null;
+    }
+  };
+
   return (
     <RepairRequestDiv>
-      <Title>출발지 선택</Title>
-      <SubTitle>고객님의 의류의 출발지를 선택해주세요.</SubTitle>
-      <SelectButton>쇼핑몰에서 보내요</SelectButton>
-      <SelectButton>우리집에서 보내요</SelectButton>
+      <Header />
+      {renderPage()}
+      {page > 1 && <Button onClick={backButtonClick}>이전 단계</Button>}
     </RepairRequestDiv>
   );
-}
+};
 
-let RepairRequestDiv = styled.div`
+const RepairRequestDiv = styled.div`
+  margin-top: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
 `;
 
-let Title = styled.div`
+const Title = styled.div`
   font-size: 40px;
   font-weight: bold;
   color: black;
   margin-bottom: 20px;
 `;
 
-let SubTitle = styled.p`
+const SubTitle = styled.p`
   color: #70737c;
   margin-bottom: 20px;
 `;
 
-let SelectButton = styled.button`
+const SelectButton = styled.button`
   color: #21242c;
   font-weight: 600;
   border-radius: 30px;
@@ -40,3 +85,9 @@ let SelectButton = styled.button`
   margin-bottom: 20px;
   background-color: white;
 `;
+
+const Button = styled.button`
+  color: #257cff;
+`;
+
+export default RepairRequest;
