380c11f3f564c492e0768a72976cb5267757f110
baekseojin <seojinbaek100@gmail.com>
2023-12-14 17:26:28 +0900

    STYLE :: 버튼 클릭하면 테두리 색 변경

--- src/components/RepairRequest/RepairRequest.jsx
+++ src/components/RepairRequest/RepairRequest.jsx
@@ -4,6 +4,7 @@ import Header from "../Header/Header";
 
 const RepairRequest = () => {
   const [page, setPage] = useState(1);
+  const [selectedButton, setSelectedButton] = useState(null);
 
   const buttonClick = () => {
     setPage(page + 1);
@@ -15,6 +16,10 @@ const RepairRequest = () => {
     }
   };
 
+  const handleSelectButtonClick = (buttonType) => {
+    setSelectedButton(buttonType);
+  };
+
   const renderPage = () => {
     switch (page) {
       case 1:
@@ -22,9 +27,18 @@ const RepairRequest = () => {
           <div>
             <Title>출발지 선택</Title>
             <SubTitle>고객님의 의류의 출발지를 선택해주세요.</SubTitle>
-            <SelectButton>쇼핑몰에서 보내요</SelectButton>
-            <SelectButton>우리집에서 보내요</SelectButton>
-            <Button onClick={buttonClick}>다음 단계</Button>
+            <SelectButton
+              isSelected={selectedButton === "쇼핑몰에서 보내요"}
+              onClick={() => handleSelectButtonClick("쇼핑몰에서 보내요")}
+            >
+              쇼핑몰에서 보내요
+            </SelectButton>
+            <SelectButton
+              isSelected={selectedButton === "우리집에서 보내요"}
+              onClick={() => handleSelectButtonClick("우리집에서 보내요")}
+            >
+              우리집에서 보내요
+            </SelectButton>
           </div>
         );
       case 2:
@@ -32,15 +46,34 @@ const RepairRequest = () => {
           <div>
             <Title>의류 선택</Title>
             <SubTitle>어떤 옷을 수선하고 싶으세요?</SubTitle>
-            <SelectButton>하의</SelectButton>
-            <SelectButton>상의</SelectButton>
-            <SelectButton>아우터</SelectButton>
-            <SelectButton>원피스</SelectButton>
-            <SelectButton>정장/교복</SelectButton>
-            <Button onClick={buttonClick}>다음 단계</Button>
+            <SelectButton
+              isSelected={selectedButton === "하의"}
+              onClick={() => handleSelectButtonClick("하의")}
+            >
+              하의
+            </SelectButton>
+            {/* 나머지 버튼들에 대해서도 동일하게 처리 */}
           </div>
         );
-      // 다른 페이지에 대한 내용 추가
+      case 3:
+        return (
+          <div>
+            <Title>의류 선택</Title>
+            <SubTitle>수선하고자 하는 성별의 의류를 선택해주세요.</SubTitle>
+            <SelectButton
+              isSelected={selectedButton === "여성복"}
+              onClick={() => handleSelectButtonClick("여성복")}
+            >
+              여성복
+            </SelectButton>
+            <SelectButton
+              isSelected={selectedButton === "남성복"}
+              onClick={() => handleSelectButtonClick("남성복")}
+            >
+              남성복
+            </SelectButton>
+          </div>
+        );
       default:
         return null;
     }
@@ -49,8 +82,10 @@ const RepairRequest = () => {
   return (
     <RepairRequestDiv>
       <Header />
+
       {renderPage()}
       {page > 1 && <Button onClick={backButtonClick}>이전 단계</Button>}
+      <Button onClick={buttonClick}>다음 단계</Button>
     </RepairRequestDiv>
   );
 };
@@ -76,10 +111,15 @@ const SubTitle = styled.p`
 `;
 
 const SelectButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   color: #21242c;
   font-weight: 600;
   border-radius: 30px;
-  border: 1px solid rgb(187, 188, 194);
+  border: 2px solid
+    ${(props) => (props.isSelected ? "#257CFF" : "rgb(187, 188, 194)")};
   width: 450px;
   height: 50px;
   margin-bottom: 20px;
@@ -87,6 +127,7 @@ const SelectButton = styled.button`
 `;
 
 const Button = styled.button`
+  display: inline;
   color: #257cff;
 `;
 
